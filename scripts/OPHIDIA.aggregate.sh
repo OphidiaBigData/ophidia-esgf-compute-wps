4d8dc32501d4dbbb6446bd9135425334c05e600f
Cosimo Palazzo <cosimo.palazzo@cmcc.it>
2019-06-16 18:48:09 +0200

    Add file download in a unique folder and simplify parameter list of OPHIDIA.aggregate.sh

--- scripts/OPHIDIA.aggregate.sh
+++ scripts/OPHIDIA.aggregate.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-cd ${2}
-ncrcat ${1} ${2}/${3}.nc
+cd ${1}/${2}
 if [ $? -ne 0 ]; then
 	exit 1
 fi
+
+ncrcat ${1}/${2}/*.nc ${1}/${2}.nc
+if [ $? -ne 0 ]; then
+	exit 2
+fi
+
+cd ${1}
+if [ $? -ne 0 ]; then
+	exit 3
+fi
+
+rm -rf ${1}/${2}
 
 exit 0
 
