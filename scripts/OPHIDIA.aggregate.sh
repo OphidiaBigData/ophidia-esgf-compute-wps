6219a2fa7993decd4143433747bde23c0ba87c11
Cosimo Palazzo <cosimo.palazzo@cmcc.it>
2019-06-17 18:39:09 +0200

    Use ncks to download file to be aggregated

--- scripts/OPHIDIA.aggregate.sh
+++ scripts/OPHIDIA.aggregate.sh
@@ -6,24 +6,30 @@
 # 3: list of input file
 # 4: input variable
 
+mkdir -p ${1}/${2}
+
 cd ${1}/${2}
 if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-for file in ${3}
+for infile in ${3}
 do
-	wget $file
+	outfile=`sed -e 's#.*/\(\)#\1#' <<< "$infile"`
+	ncks -h --no-abc -O $infile $outfile
+	if [ $? -ne 0 ]; then
+		exit 2
+	fi
 done
 
 ncrcat -O -v ${4} ${1}/${2}/*.nc ${1}/${2}.nc
 if [ $? -ne 0 ]; then
-	exit 2
+	exit 3
 fi
 
 cd ${1}
 if [ $? -ne 0 ]; then
-	exit 3
+	exit 4
 fi
 
 rm -rf ${1}/${2}
