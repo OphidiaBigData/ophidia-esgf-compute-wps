8ff725f7b4208301316aa848d0484b8d467c57a5
Cosimo Palazzo <cosimo.palazzo@cmcc.it>
2019-06-28 17:12:41 +0200

    Amend to previous commit

--- security/test.py
+++ security/test.py
@@ -26,3 +26,5 @@ print(headers)
 resp = requests.get("{}/ows/proxy/esgf-cwt-wps?service=WPS&version=1.0.0&request=Execute&identifier=OPHIDIA.subset&datainputs=variable%3D%5B%7B%22uri%22%3A%22file.nc%22%2C%22id%22%3A%22tos%7Cv1%22%2C%22domain%22%3A%22d0%22%7D%5D%3Bdomain%3D%5B%7B%22lat%22%3A%7B%22start%22%3A-60%2C%22end%22%3A-15%2C%22crs%22%3A%22values%22%7D%2C%22lon%22%3A%7B%22start%22%3A42%2C%22end%22%3A113%2C%22crs%22%3A%22values%22%7D%2C%22id%22%3A%22d0%22%7D%5D%3Boperation%3D%5B%7B%22name%22%3A%22OPHIDIA.subset%22%2C%22input%22%3A%5B%22v1%22%5D%2C%22domain%22%3A%22d0%22%2C%22axes%22%3A%22time%22%7D%5D".format(base_url), headers=headers, verify=False)
 resp.ok
 
+resp.text
+
